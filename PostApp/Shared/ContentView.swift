853c09d6c2927e54cc1f8cf861fcc4798685198c
Gustavo Rodrigues Leitão <leitaozu@gmail.com>
2022-01-18 16:56:30 -0300

    feat: commit initial

---
+++ PostApp/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Gustavo Rodrigues Leitão on 18/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PostApp/Shared/PostAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostAppApp.swift
+//  Shared
+//
+//  Created by Gustavo Rodrigues Leitão on 18/01/22.
+//
+
+import SwiftUI
+
+@main
+struct PostAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
